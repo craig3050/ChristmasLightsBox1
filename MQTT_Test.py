69d25287e4f7818bbc41ba84342a9c2ba38a484e
Craig Cuninghame <craig3050@gmail.com>
2017-11-23 22:11:50 +0000

    fixed bracket position

--- MQTT_Test.py
+++ MQTT_Test.py
@@ -1,25 +1,27 @@
 import paho.mqtt.publish as publish
 import time
 
-def colour_change(R, G, B):
-    colour = "{\"r\":" + str(R) + ",\"g\":" + str(G) + ",\"b\":" + str(B) + ",\"effect\":\"solid\"}"
+area_name = "test1"
+
+def colour_change(area_name, R, G, B):
+    colour = "{\"r\":" + str(R) + ",\"g\":" + str(G) + ",\"b\":" + str(B) + "},\"effect\":\"solid\""
     message = "{\"state\":\"ON\",\"color\":" + colour + "}"
-    publish.single("test1", message, hostname="192.168.0.14")
+    publish.single(area_name, message, hostname="192.168.0.14")
 
-def brightness(brightness):
+def brightness(area_name, brightness):
     message = "{\"state\":\"ON\",\"brightness\":" + str(brightness) + "}"
-    publish.single("test1", message, hostname="192.168.0.14")
+    publish.single(area_name, message, hostname="192.168.0.14")
 
-def effect_change(effect_name):
+def effect_change(area_name, effect_name):
     print("none")
 
 def main():
     bright_value = input("Enter Brightness Value to Send: ")
-    brightness(bright_value)
+    brightness(area_name, bright_value)
     R = input("Enter R: ")
     G = input("Enter G: ")
     B = input("Enter B: ")
-    colour_change(R, G, B)
+    colour_change(area_name, R, G, B)
 
 
 if __name__ == '__main__':
