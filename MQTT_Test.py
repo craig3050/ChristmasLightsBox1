f09f442b93ee4345162f84a2dc56a5590e4755df
Craig Cuninghame <craig3050@gmail.com>
2017-11-23 22:05:26 +0000

    Added more publish values

--- MQTT_Test.py
+++ MQTT_Test.py
@@ -1,10 +1,25 @@
 import paho.mqtt.publish as publish
 import time
 
-def main():
-    brightness = input("Enter Brightness Value to Send: ")
+def colour_change(R, G, B):
+    colour = "{\"r\":" + str(R) + ",\"g\":" + str(G) + ",\"b\":" + str(B) + ",\"effect\":\"solid\"}"
+    message = "{\"state\":\"ON\",\"color\":" + colour + "}"
+    publish.single("test1", message, hostname="192.168.0.14")
+
+def brightness(brightness):
     message = "{\"state\":\"ON\",\"brightness\":" + str(brightness) + "}"
     publish.single("test1", message, hostname="192.168.0.14")
+
+def effect_change(effect_name):
+    print("none")
+
+def main():
+    bright_value = input("Enter Brightness Value to Send: ")
+    brightness(bright_value)
+    R = input("Enter R: ")
+    G = input("Enter G: ")
+    B = input("Enter B: ")
+    colour_change(R, G, B)
 
 
 if __name__ == '__main__':
